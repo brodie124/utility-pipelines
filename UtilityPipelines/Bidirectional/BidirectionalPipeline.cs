421bc5169b71ebbf43da07aca95d8eaca1fb5667
Brodie Pestell <brodie.pestell7@gmail.com>
2023-08-28 19:44:43 +0100

    Add XML documentation

--- UtilityPipelines/Bidirectional/BidirectionalPipeline.cs
+++ UtilityPipelines/Bidirectional/BidirectionalPipeline.cs
@@ -1,20 +1,52 @@
 namespace UtilityPipelines.Bidirectional;
 
+/// <summary>
+/// Bi-directional pipeline taking in one input and providing exactly one output.
+/// Asynchronous and synchronous execution allowed.
+/// </summary>
+/// <typeparam name="TInput">Input of the first step in the pipeline</typeparam>
+/// <typeparam name="TOutput">Output from the last step in the pipeline</typeparam>
 public interface IBidirectionalPipeline<TInput, TOutput>
 {
+    /// <summary>
+    /// Asynchronously executes the pipeline with the provided input.
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
     public Task<TOutput> ExecuteAsync(TInput input);
+    
+    /// <summary>
+    /// Synchronously executes the pipeline with the provided input.
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
     public TOutput Execute(TInput input);
 }
 
+/// <summary>
+/// Bi-Directional pipeline implementation - taking in one input and providing exactly one output.
+/// Capable of both synchronous and asynchronous execution.
+/// </summary>
+/// <typeparam name="TFirstInput">Input of the first step in the pipeline</typeparam>
+/// <typeparam name="TLastOutput">Output from the last step in the pipeline</typeparam>
 public class BidirectionalPipeline<TFirstInput, TLastOutput> : IBidirectionalPipeline<TFirstInput, TLastOutput>
 {
     private readonly ICollection<Func<object, Task<object>>> _pipelineSteps;
 
+    /// <summary>
+    /// Creates a bi-directional pipeline based on the provided list of steps.
+    /// </summary>
+    /// <param name="pipelineSteps"></param>
     public BidirectionalPipeline(ICollection<Func<object, Task<object>>> pipelineSteps)
     {
         _pipelineSteps = pipelineSteps;
     }
 
+    /// <summary>
+    /// Executes the pipeline asynchronously - whilst still remaining in sequence.
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
     public async Task<TLastOutput> ExecuteAsync(TFirstInput input)
     {
         var nextInput = input as object;
@@ -26,6 +58,11 @@ public class BidirectionalPipeline<TFirstInput, TLastOutput> : IBidirectionalPip
         return (TLastOutput)nextInput!;
     }
     
+    /// <summary>
+    /// Executes the pipeline synchronously.
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
     public TLastOutput Execute(TFirstInput input)
     {
         return ExecuteAsync(input).Result;
--- UtilityPipelines/Bidirectional/BidirectionalPipelineBuilder.cs
+++ UtilityPipelines/Bidirectional/BidirectionalPipelineBuilder.cs
@@ -1,21 +1,52 @@
 namespace UtilityPipelines.Bidirectional;
 
+/// <summary>
+/// Dynamically creates a bi-directional pipeline from a list of steps defined in a fluent manner.
+/// </summary>
+/// <typeparam name="TFirstInput">The input to the first step of the pipeline</typeparam>
+/// <typeparam name="TLastOutput">The output from the last step of the pipeline</typeparam>
+/// <typeparam name="TNextInput">The input into the NEXT step of the pipeline (the output of the previous step)</typeparam>
 public interface IBidirectionalPipelineBuilder<TFirstInput, TLastOutput, TNextInput>
 {
+    /// <summary>
+    /// Add a step to the pipeline.
+    /// </summary>
+    /// <param name="input">Lambda to be executed</param>
+    /// <typeparam name="TOutput"></typeparam>
+    /// <returns></returns>
     IBidirectionalPipelineBuilder<TFirstInput, TLastOutput, TOutput> AddStep<TOutput>(Func<TNextInput, TOutput> input);
+    
+    /// <summary>
+    /// Creates a bi-directional pipeline using the already-defined steps.
+    /// </summary>
+    /// <returns></returns>
     IBidirectionalPipeline<TFirstInput, TLastOutput> CreatePipeline();
 }
 
+/// <summary>
+/// Dynamically creates a bi-directional pipeline from a list of steps defined in a fluent manner.
+/// </summary>
 public class BidirectionalPipelineBuilder<TFirstInput, TLastOutput, TNextInput>
     : IBidirectionalPipelineBuilder<TFirstInput, TLastOutput, TNextInput>
 {
     private readonly ICollection<Func<object, Task<object>>> _pipelineStepList;
 
-    protected BidirectionalPipelineBuilder(ICollection<Func<object, Task<object>>>? pipelineStepList = null)
+    /// <summary>
+    /// Creates a bi-directional pipeline builder from an existing step of steps.
+    /// If no existing list of steps is provided, a new collection is created.
+    /// </summary>
+    /// <param name="pipelineStepList">Existing collection of steps</param>
+    protected BidirectionalPipelineBuilder(ICollection<Func<object, Task<object>>>? pipelineStepList)
     {
         _pipelineStepList = pipelineStepList ?? new List<Func<object, Task<object>>>();
     }
 
+    /// <summary>
+    /// Adds a step to the pipeline
+    /// </summary>
+    /// <param name="input"></param>
+    /// <typeparam name="TOutput"></typeparam>
+    /// <returns></returns>
     public IBidirectionalPipelineBuilder<TFirstInput, TLastOutput, TOutput> AddStep<TOutput>(
         Func<TNextInput, TOutput> input
     )
@@ -24,6 +55,11 @@ public class BidirectionalPipelineBuilder<TFirstInput, TLastOutput, TNextInput>
         return new BidirectionalPipelineBuilder<TFirstInput, TLastOutput, TOutput>(_pipelineStepList);
     }
 
+    /// <summary>
+    /// Creates an instance of IBidirectionalPipeline based on the previously defined steps.
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="InvalidOperationException"></exception>
     public IBidirectionalPipeline<TFirstInput, TLastOutput> CreatePipeline()
     {
         if (typeof(TNextInput) != typeof(TLastOutput))
@@ -33,11 +69,16 @@ public class BidirectionalPipelineBuilder<TFirstInput, TLastOutput, TNextInput>
     }
 }
 
+/// <summary>
+/// Dynamically creates a bi-directional pipeline from a list of steps defined in a fluent manner.
+/// </summary>
 public class BidirectionalPipelineBuilder<TFirstInput, TLastOutput>
     : BidirectionalPipelineBuilder<TFirstInput, TLastOutput, TFirstInput>
 {
-    public BidirectionalPipelineBuilder(ICollection<Func<object, Task<object>>>? pipelineStepList = null)
-        : base(pipelineStepList)
+    /// <summary>
+    /// Creates a bi-directional pipeline builder.
+    /// </summary>
+    public BidirectionalPipelineBuilder() : base(null)
     {
     }
 }
